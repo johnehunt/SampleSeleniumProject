454e2eec8d3926e696df96a8868d98c8a2a00a96
John Hunt <jeh@midmarsh.co.uk>
2020-05-05 17:47:29 +0100

    Sample set of Selenium tests written in JUnit - assumes you will download chromedriver for your platform

---
+++ src/test/java/com/jjh/sample/SeleniumJUnitTest.java
@@ -0,0 +1,40 @@
+package com.jjh.sample;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class SeleniumJUnitTest {
+
+	private static SeleniumConfig config;
+
+	@BeforeAll
+	public static void setUp() {
+		config = new SeleniumConfig();
+		config.getDriver().get("http://localhost:8080/index.html");
+	}
+
+	@AfterAll
+	public static void tearDown() {
+		config.close();
+	}
+
+	@Test
+	public void check_page_title_is_Hello_World() {
+		String actualTitle = config.getDriver().getTitle();
+		assertEquals("Hello World", actualTitle);
+	}
+
+	@Test
+	public void check_heading_is_Welcome() {
+		String heading = 
+				config.getDriver()
+				      .findElement(By.className("heading"))
+				      .getText();
+		assertEquals("Welcome", heading);
+	}
+
+}
