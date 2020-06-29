e54612636b6fbc305107b3c240c55e3e5ffd1481
John Hunt <john.hunt10@gmail.com>
2020-06-29 12:41:39 +0100

    Added comments

--- src/test/java/com/jjh/sample/SeleniumConfig.java
+++ src/test/java/com/jjh/sample/SeleniumConfig.java
@@ -5,16 +5,24 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+/**
+ * Provides a generic SeleniumConfig class
+ * that can be referenced by numerous Selenium Test classes.
+ */
 public class SeleniumConfig {
+
+	/** The Drive instance **/
 	private WebDriver driver;
 
 	public SeleniumConfig() {
+		// Set up thew appropriate Web Drive instance
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
+	// Statics are executed when the class is loaded
 	static {
 		// Setting system properties of ChromeDriver
 		// System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
