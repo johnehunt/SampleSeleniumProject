a9ffcf67bf43b31dfc67f3b34652eb00dab94517
John Hunt <john.hunt10@gmail.com>
2020-06-29 12:45:07 +0100

    Added comments

--- src/test/java/com/jjh/sample/SeleniumJUnitTest.java
+++ src/test/java/com/jjh/sample/SeleniumJUnitTest.java
@@ -13,17 +13,21 @@ public class SeleniumJUnitTest {
 
 	@BeforeAll
 	public static void setUp() {
+		// Instantiate config object - start the web driver
 		config = new SeleniumConfig();
+		// Load the web page to be used for all tests
 		config.getDriver().get("http://localhost:8080/index.html");
 	}
 
 	@AfterAll
 	public static void tearDown() {
+		// Shut down the Selenium web driver
 		config.close();
 	}
 
 	@Test
 	public void check_page_title_is_Hello_World() {
+		// Get hold of title of web page
 		String actualTitle = config.getDriver().getTitle();
 		assertEquals("Hello World", actualTitle);
 	}
